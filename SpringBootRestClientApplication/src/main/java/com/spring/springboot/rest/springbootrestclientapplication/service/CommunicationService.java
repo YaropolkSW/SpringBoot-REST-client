149a6427c0adcd0a75a966ad1ac299f0c1c3ef95
Vyacheslav Yugzov <Yugzov96@mail.ru>
2023-01-03 17:11:31 +0400

    Try to make tests run without connection to internet

--- SpringBootRestClientApplication/src/main/java/com/spring/springboot/rest/springbootrestclientapplication/service/CommunicationService.java
+++ SpringBootRestClientApplication/src/main/java/com/spring/springboot/rest/springbootrestclientapplication/service/CommunicationService.java
@@ -1,46 +1,49 @@
 package com.spring.springboot.rest.springbootrestclientapplication.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.spring.springboot.rest.springbootrestclientapplication.dto.Response;
-import lombok.NoArgsConstructor;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
-@NoArgsConstructor
-@CacheConfig(cacheNames = "data")
+@CacheConfig
 public class CommunicationService {
     private final static String STRING_URL =
             "https://nominatim.openstreetmap.org/search?q=%s&country=russia&format=json&polygon_geojson=1";
     private final static String SPACE = " ";
     private final static String UNDERLINE = "_";
 
-    private final static String WRONG_URL = "Wrong URL!";
-    private final static String CANT_READ_FROM_URL = "Can't read data from URL!";
+    private final static String CANT_READ_FROM_RESPONSE_ENTITY = "Can't read data from Response Entity!";
 
-    @Cacheable
+    private final ObjectMapper mapper;
+    private final RestTemplate restTemplate;
+
+    public CommunicationService(
+        final ObjectMapper mapper,
+        final RestTemplate restTemplate
+    ) {
+        this.mapper = mapper;
+        this.restTemplate = restTemplate;
+    }
+
+    @Cacheable(value = "data", key = "#placeName")
     public Response getData(final String placeName) {
-        URL url = null;
-        try {
-            url = new URL(String.format(STRING_URL, placeName.replaceAll(SPACE, UNDERLINE)));
-        } catch (MalformedURLException e) {
-            System.out.println(WRONG_URL);
-        }
+        final String url = String.format(STRING_URL, placeName.replaceAll(SPACE, UNDERLINE));
+        final String responseEntity = restTemplate.getForObject(url, String.class);
 
         List<Response> responses = null;
         try {
-            responses = new ObjectMapper().readValue(url, ArrayList.class);
-        } catch (IOException e) {
-            System.out.println(CANT_READ_FROM_URL);
+            responses = mapper.readValue(responseEntity, new TypeReference<List<Response>>() {});
+        } catch (JsonProcessingException e) {
+            System.out.println(CANT_READ_FROM_RESPONSE_ENTITY);
         }
 
-        return new ObjectMapper().convertValue(responses.get(0), Response.class);
+        return responses.get(0);
     }
 }
--- SpringBootRestClientApplication/src/test/java/com/spring/springboot/rest/springbootrestclientapplication/SpringBootRestClientApplicationTests.java
+++ SpringBootRestClientApplication/src/test/java/com/spring/springboot/rest/springbootrestclientapplication/SpringBootRestClientApplicationTests.java
@@ -1,5 +1,6 @@
 package com.spring.springboot.rest.springbootrestclientapplication;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.spring.springboot.rest.springbootrestclientapplication.dto.CoordinatePoint;
 import com.spring.springboot.rest.springbootrestclientapplication.dto.Geojson;
 import com.spring.springboot.rest.springbootrestclientapplication.dto.Response;
@@ -7,13 +8,20 @@ import com.spring.springboot.rest.springbootrestclientapplication.service.Commun
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.cache.CacheManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -24,20 +32,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 class SpringBootRestClientApplicationTests {
-    private final MockMvc mockMvc;
-    private final CacheManager cacheManager;
-    private final CommunicationService communicationService;
+    private final static String URL =
+        "https://nominatim.openstreetmap.org/search?q=Samara&country=russia&format=json&polygon_geojson=1";
+    private final static String PATH_TO_RESPONSE_BODY = "src/test/resources/response-body.txt";
+    private final static String EMPTY_LINE = "";
 
     @Autowired
-    public SpringBootRestClientApplicationTests(
-        final MockMvc mockMvc,
-        final CacheManager cacheManager,
-        final CommunicationService communicationService
-    ) {
-        this.mockMvc = mockMvc;
-        this.cacheManager = cacheManager;
-        this.communicationService = communicationService;
-    }
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper mapper;
+    @Autowired
+    private CacheManager cacheManager;
+    @MockBean
+    private RestTemplate restTemplate;
+    private final CommunicationService communicationService = new CommunicationService(mapper, restTemplate);
 
     @Test
     public void EnableCachingShouldWorkProperly() throws Exception {
@@ -59,9 +67,15 @@ class SpringBootRestClientApplicationTests {
     }
 
     @Test
-    public void ServiceMethodGetDataShouldReturnCorrectResponse() {
+    public void ServiceMethodGetDataShouldReturnCorrectResponse() throws IOException {
+        String responseBody = String.join(EMPTY_LINE, Files.readAllLines(Paths.get(PATH_TO_RESPONSE_BODY)));
+
+        Mockito
+            .when(restTemplate.getForObject(URL, String.class))
+            .thenReturn(responseBody);
+
         final CoordinatePoint coordinatePoint = CoordinatePoint.builder()
-            .longitude(50.138)
+            .longitude(50.136)
             .latitude(53.337)
             .build();
 
@@ -71,7 +85,7 @@ class SpringBootRestClientApplicationTests {
             .build();
 
         final Response expectedResponse = Response.builder()
-            .name("Самара, городской округ Самара, Самарская область, Приволжский федеральный округ, 443028, Россия")
+            .name("городской округ Самара, Самарская область, Приволжский федеральный округ, Россия")
             .geojson(geojson)
             .build();
 
