77ca09fba49eb85cba704d0e623df1c1e4f57659
Vyacheslav Yugzov <Yugzov96@mail.ru>
2023-01-03 18:52:23 +0400

    Reworked test scenaries to make them work without internet connection

--- SpringBootRestClientApplication/src/test/java/com/spring/springboot/rest/springbootrestclientapplication/CommunicationControllerTest.java
+++ SpringBootRestClientApplication/src/test/java/com/spring/springboot/rest/springbootrestclientapplication/CommunicationControllerTest.java
@@ -1,20 +1,27 @@
 package com.spring.springboot.rest.springbootrestclientapplication;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+import com.spring.springboot.rest.springbootrestclientapplication.controller.CommunicationController;
 import com.spring.springboot.rest.springbootrestclientapplication.dto.CoordinatePoint;
 import com.spring.springboot.rest.springbootrestclientapplication.dto.Geojson;
 import com.spring.springboot.rest.springbootrestclientapplication.dto.Response;
 import com.spring.springboot.rest.springbootrestclientapplication.service.CommunicationService;
+import lombok.NoArgsConstructor;
 import org.junit.Assert;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.cache.CacheManager;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.client.RestTemplate;
@@ -25,30 +32,35 @@ import java.nio.file.Paths;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
-class SpringBootRestClientApplicationTests {
+public class CommunicationControllerTest {
     private final static String URL =
         "https://nominatim.openstreetmap.org/search?q=Samara&country=russia&format=json&polygon_geojson=1";
-    private final static String PATH_TO_RESPONSE_BODY = "src/test/resources/response-body.txt";
+    private final static String PATH_TO_RESPONSE_BODY = "src/test/resources/response-body.json";
     private final static String EMPTY_LINE = "";
 
+    @MockBean
+    private RestTemplate restTemplate;
     @Autowired
     private MockMvc mockMvc;
     @Autowired
-    private ObjectMapper mapper;
-    @Autowired
     private CacheManager cacheManager;
-    @MockBean
-    private RestTemplate restTemplate;
-    private final CommunicationService communicationService = new CommunicationService(mapper, restTemplate);
+    @Autowired
+    private CommunicationService communicationService;
 
     @Test
     public void EnableCachingShouldWorkProperly() throws Exception {
+        final String responseString =
+            String.join(EMPTY_LINE, Files.readAllLines(Paths.get(PATH_TO_RESPONSE_BODY)));
+
+        Mockito.when(restTemplate.getForObject(URL, String.class)).thenReturn(responseString);
+
         this.mockMvc
             .perform(get("/info?placeName=Samara"))
             .andDo(print())
@@ -59,6 +71,11 @@ class SpringBootRestClientApplicationTests {
 
     @Test
     public void ControllerMethodGetDataShouldReturnStatus200AndCorrectView() throws Exception {
+        final String responseString =
+            String.join(EMPTY_LINE, Files.readAllLines(Paths.get(PATH_TO_RESPONSE_BODY)));
+
+        Mockito.when(restTemplate.getForObject(URL, String.class)).thenReturn(responseString);
+
         this.mockMvc
             .perform(get("/info?placeName=Samara"))
             .andDo(print())
@@ -68,11 +85,10 @@ class SpringBootRestClientApplicationTests {
 
     @Test
     public void ServiceMethodGetDataShouldReturnCorrectResponse() throws IOException {
-        String responseBody = String.join(EMPTY_LINE, Files.readAllLines(Paths.get(PATH_TO_RESPONSE_BODY)));
+        final String responseString =
+            String.join(EMPTY_LINE, Files.readAllLines(Paths.get(PATH_TO_RESPONSE_BODY)));
 
-        Mockito
-            .when(restTemplate.getForObject(URL, String.class))
-            .thenReturn(responseBody);
+        Mockito.when(restTemplate.getForObject(URL, String.class)).thenReturn(responseString);
 
         final CoordinatePoint coordinatePoint = CoordinatePoint.builder()
             .longitude(50.136)
